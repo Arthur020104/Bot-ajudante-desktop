555589b098679a0ce577e6086b24e48e7399be27
Arthur020104 <arthur020104@gmail.com>
2022-07-30 21:48:03 +0000

    adicionando funcionalidade de limpar terminal no fim da instalacao e adicionando bibliotecas necessarias que estavam faltando

--- instalarequirements.py
+++ instalarequirements.py
@@ -1,11 +1,21 @@
+from time import sleep
 import subprocess
-requirements = ["opencv-python","google","playsound", "pyttsx3", "speech_recognition","datetime"]
+requirements = ["opencv-python","google","playsound", "pyttsx3", "SpeechRecognition","datetime","pipwin"]
 #playsound ctypes
 def run(cmd):
     subprocess.run(["powershell", "-Command", cmd])
 def principal():
     for requirement in requirements:
         run(f"pip install {requirement}")
-
+    run(f"pipwin install pyaudio")
+    for i in range(3, -1,-1):
+        if i == 0:
+            print("Limpando...")
+            run("cls")
+        if i in range(1,4):
+            if i == 3:
+                print("Limpando o terminal em.")
+            print(i)
+        sleep(1)
 if __name__ == "__main__":
     principal()
